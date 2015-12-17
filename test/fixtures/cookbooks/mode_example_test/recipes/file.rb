8d7768f537ac4ebd9cf531fe815b72cad4022c1e
Kevin J. Dickerson <kevin.dickerson@ge.com>
2015-12-16 22:06:25 -0800

    Clarify types

--- test/fixtures/cookbooks/mode_example_test/recipes/file.rb
+++ test/fixtures/cookbooks/mode_example_test/recipes/file.rb
@@ -28,31 +28,31 @@ file '/home/vagrant/string_04755.txt' do
   group 'vagrant'
 end
 
-file '/home/vagrant/integer_655.txt' do # ignore FC006
+file '/home/vagrant/fixnum_655.txt' do # ignore FC006
   mode 655
   user 'vagrant'
   group 'vagrant'
 end
 
-file '/home/vagrant/integer_0655.txt' do
+file '/home/vagrant/fixnum_0655.txt' do
   mode 0655
   user 'vagrant'
   group 'vagrant'
 end
 
-file '/home/vagrant/integer_00655.txt' do
+file '/home/vagrant/fixnum_00655.txt' do
   mode 00655
   user 'vagrant'
   group 'vagrant'
 end
 
-file '/home/vagrant/integer_000655.txt' do
+file '/home/vagrant/fixnum_000655.txt' do
   mode 000655
   user 'vagrant'
   group 'vagrant'
 end
 
-file '/home/vagrant/integer_04755.txt' do
+file '/home/vagrant/fixnum_04755.txt' do
   mode 04755
   user 'vagrant'
   group 'vagrant'
--- test/integration/mode_example_test/serverspec/mode_example_test_spec.rb
+++ test/integration/mode_example_test/serverspec/mode_example_test_spec.rb
@@ -53,52 +53,52 @@ describe 'mode_example_test::file' do
     end
 
     describe 'with Fixnum parameters' do
-      it '/home/vagrant/integer_655.txt is in the correct mode' do
-        integer_655 = file '/home/vagrant/integer_655.txt'
-        expect(integer_655).to be_mode 1217 # this may be unexpected
-        expect(integer_655).to_not be_mode 655 # this may be unexpected
-        expect(integer_655).to_not be_mode 01217
-        expect(integer_655).to_not be_mode 0o1217 # this may be unexpected
-        expect(integer_655).to_not be_mode 0655
-        expect(integer_655).to_not be_mode 429
-        expect(integer_655).to_not be_mode '0655'
+      it '/home/vagrant/fixnum_655.txt is in the correct mode' do
+        fixnum_655 = file '/home/vagrant/fixnum_655.txt'
+        expect(fixnum_655).to be_mode 1217 # this may be unexpected
+        expect(fixnum_655).to_not be_mode 655 # this may be unexpected
+        expect(fixnum_655).to_not be_mode 01217
+        expect(fixnum_655).to_not be_mode 0o1217 # this may be unexpected
+        expect(fixnum_655).to_not be_mode 0655
+        expect(fixnum_655).to_not be_mode 429
+        expect(fixnum_655).to_not be_mode '0655'
       end
 
-      it '/home/vagrant/integer_0655.txt is in the correct mode' do
-        integer_0655 = file '/home/vagrant/integer_0655.txt'
-        expect(integer_0655).to be_mode '655'
-        expect(integer_0655).to be_mode 655
-        expect(integer_0655).to be_mode 01217
-        expect(integer_0655).to be_mode 0o1217
-        expect(integer_0655).to_not be_mode 0655
-        expect(integer_0655).to_not be_mode 429
-        expect(integer_0655).to_not be_mode '0655'
+      it '/home/vagrant/fixnum_0655.txt is in the correct mode' do
+        fixnum_0655 = file '/home/vagrant/fixnum_0655.txt'
+        expect(fixnum_0655).to be_mode '655'
+        expect(fixnum_0655).to be_mode 655
+        expect(fixnum_0655).to be_mode 01217
+        expect(fixnum_0655).to be_mode 0o1217
+        expect(fixnum_0655).to_not be_mode 0655
+        expect(fixnum_0655).to_not be_mode 429
+        expect(fixnum_0655).to_not be_mode '0655'
       end
 
-      it '/home/vagrant/integer_00655.txt is in the correct mode' do
-        integer_00655 = file '/home/vagrant/integer_00655.txt'
-        expect(integer_00655).to be_mode '655'
-        expect(integer_00655).to be_mode 655
-        expect(integer_00655).to_not be_mode 00655
-        expect(integer_00655).to_not be_mode '00655'
+      it '/home/vagrant/fixnum_00655.txt is in the correct mode' do
+        fixnum_00655 = file '/home/vagrant/fixnum_00655.txt'
+        expect(fixnum_00655).to be_mode '655'
+        expect(fixnum_00655).to be_mode 655
+        expect(fixnum_00655).to_not be_mode 00655
+        expect(fixnum_00655).to_not be_mode '00655'
       end
 
-      it '/home/vagrant/integer_000655.txt is in the correct mode' do
-        integer_000655 = file '/home/vagrant/integer_000655.txt'
-        expect(integer_000655).to be_mode '655'
-        expect(integer_000655).to be_mode 655
-        expect(integer_000655).to_not be_mode 000655
-        expect(integer_000655).to_not be_mode '000655'
+      it '/home/vagrant/fixnum_000655.txt is in the correct mode' do
+        fixnum_000655 = file '/home/vagrant/fixnum_000655.txt'
+        expect(fixnum_000655).to be_mode '655'
+        expect(fixnum_000655).to be_mode 655
+        expect(fixnum_000655).to_not be_mode 000655
+        expect(fixnum_000655).to_not be_mode '000655'
       end
 
-      it '/home/vagrant/integer_04755.txt is in the correct mode' do
-        integer_04755 = file '/home/vagrant/integer_04755.txt'
-        expect(integer_04755).to be_mode '4755'
-        expect(integer_04755).to be_mode 4755
-        expect(integer_04755).to be_mode 0o11223
-        expect(integer_04755).to_not be_mode 0755
-        expect(integer_04755).to_not be_mode 2541
-        expect(integer_04755).to_not be_mode '0755'
+      it '/home/vagrant/fixnum_04755.txt is in the correct mode' do
+        fixnum_04755 = file '/home/vagrant/fixnum_04755.txt'
+        expect(fixnum_04755).to be_mode '4755'
+        expect(fixnum_04755).to be_mode 4755
+        expect(fixnum_04755).to be_mode 0o11223
+        expect(fixnum_04755).to_not be_mode 0755
+        expect(fixnum_04755).to_not be_mode 2541
+        expect(fixnum_04755).to_not be_mode '0755'
       end
     end
   end
