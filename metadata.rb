8cfc0bbf8c4f7b0b2c589f1f833ea7e8da223e6b
Kevin J. Dickerson <kevin.dickerson@loom.technology>
2016-03-30 23:43:16 -0700

    Clean up cookbook metadata

--- metadata.rb
+++ metadata.rb
@@ -1,18 +1,13 @@
 name 'mode_example'
-maintainer 'Kevin Dickerson'
-maintainer_email 'kdickerson@chef.io'
+maintainer 'Kevin J. Dickerson'
+maintainer_email 'kevin.dickerson@loom.technology'
 license 'Apache 2.0'
-description 'Provides tests to show unexpected behavior for resource modes'
+description 'Contains tests to show unexpected behavior for the mode ' \
+            'property on a Chef resource'
 long_description IO.read File.join File.dirname(__FILE__), 'README.md'
 version '0.1.0'
 
 supports 'ubuntu', '14.04'
-supports 'centos', '6.7'
-supports 'centos', '7.1'
 
-depends 'apt'
-
-source_url 'https://github.com/kevindickerson/mode_example' if \
-  respond_to?(:source_url)
-issues_url 'https://github.com/kevindickerson/mode_example/issues' if \
-  respond_to?(:issues_url)
+source_url 'https://github.com/kevindickerson/mode_example'
+issues_url 'https://github.com/kevindickerson/mode_example/issues'
